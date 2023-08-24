c11977fb383d738c9d30018caf8d2743539a9b56
BakaDream <2507457629@qq.com>
2023-08-24 22:25:53 +0800

    初始化redis

--- global/global.go
+++ global/global.go
@@ -1,10 +1,13 @@
 package global
 
 import (
+	"WheelChair-tiktok/cache"
+	"WheelChair-tiktok/utils/storage"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 var Logger *zap.Logger
-
 var DB *gorm.DB
+var RedisClient *cache.RedisClient
+var Storage storage.Store
