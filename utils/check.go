088496253a852319a2f10ae0b0af853542e30943
BakaDream <bakameng@qq.com>
2023-08-29 00:08:15 +0800

    fix bugs

--- utils/check.go
+++ utils/check.go
@@ -11,7 +11,7 @@ params: fileName 文件名
 return: bool 是true 非false
 */
 func IsVideoFile(fileName string) bool {
-	videoExtensions := []string{".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".webm", ".m4v"}
+	videoExtensions := []string{"mp4", "avi", "mkv", "mov", "wmv", "flv", "webm", "m4v"}
 
 	ext := strings.ToLower(fileName[strings.LastIndex(fileName, "."):])
 	for _, videoExt := range videoExtensions {
