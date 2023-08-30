6b9257bef1f04f0eeee1ec35cf608e0c1fed3cf9
BakaDream <bakameng@qq.com>
2023-08-30 17:34:36 +0800

    添加注解

--- model/follow.go
+++ model/follow.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Follow struct {
 	gorm.Model
-	FollowedID uint //被关注人的ID
-	FollowerID uint //关注者ID
+	FollowedID uint `gorm:"followed_id;index"` //被关注人的ID
+	FollowerID uint `gorm:"follower_id;index"` //关注者ID
 }
 
 func (Follow) TableName() string {
